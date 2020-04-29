02a0e3c7bf385097f743f963cd688d09e37d164b
Zach Southwell <zach@southwellhome.com>
2020-04-29 11:13:13 -0500

    Fix some build issues

--- src/edu/utexas/testing/ee382c/CodeCoverage.java
+++ src/edu/utexas/testing/ee382c/CodeCoverage.java
@@ -20,8 +20,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
